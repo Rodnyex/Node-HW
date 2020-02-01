98b0aac211cc500d61da79dcef5648aa9484acc4
Rodny Excellent <rodnyexcellent@gmail.com>
2020-02-01 11:58:15 -0500

    want to see if my commits show

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const axios = require('axios');
 axios.get('https://omdbapi.com/?t=batman&apikey=trilogy')
    .then(res => console.log(res))
    .catch(err => console.log(err))
+
+   //adding this just for the hell of it because i want to figure out what is going on with my commits
    
